191ce7ddc0e19ce5b3d77d06f5b002028001662d
jianke0604 <galaxy-1@sjtu.edu.cn>
2024-04-17 20:25:44 +0800

    add echarts and people distrubution

--- src/components/Bgpage.vue
+++ src/components/Bgpage.vue
@@ -8,16 +8,24 @@
                         title="检索热度"
                         :boxb="true"
                     >
-                    <div class="word-cloud">
-                        <div v-for="(item, index) in heatData" :key="index" :style="{ fontSize: item[1] + 'px', left: Math.random() * 75 + '%', top: Math.random() * 60 + '%' }">{{ item[0] }} </div>
-                    </div>
+                        <div class="word-cloud">
+                            <div v-for="(item, index) in heatData" :key="index" :style="{ fontSize: item[1] + 'px', left: Math.random() * 75 + '%', top: Math.random() * 60 + '%' }">{{ item[0] }} </div>
+                        </div>
                     </Viewbox>
                 </div>
                 <div class="box">
+                    <!-- 月份和日期输入框 -->
+                    <div>
+                        月份：<input type="text" v-model="month" @input="updateDataStr">
+                        日期：<input type="text" v-model="day" @input="updateDataStr"><br>
+                    </div>
                     <Viewbox
-                        :title="subTitle"
+                        title="1月1日实时在馆人数分布"
                         :boxb="true"
-                    />
+                    >
+                        <!-- 曲线图容器 -->
+                        <div id="attendanceChart" style="width: 100%; height: 100%;"></div>
+                    </Viewbox>
                 </div>
                 <div class="box">
                     <Viewbox
@@ -40,7 +48,6 @@
                     />
                 </div>
             </div>
-            
         </div>
     </div>
 </template>
@@ -48,6 +55,8 @@
 <script>
 import Header from './Header.vue'
 import Viewbox from './viewbox/Viewbox.vue'
+import * as echarts from 'echarts';
+
 export default {
     components: {
         Header,
@@ -56,12 +65,17 @@ export default {
     data() {
         return {
             subTitle: "子标题",
-            heatData: []
+            heatData: [], // 保存检索热度数据
+            libraryAttendanceData: [], // 新建全年在馆人数数据
+            month: '1', // 默认月份为1
+            day: '11', // 默认日期为1
         }
     },
     mounted() {
         // Load the heat data when the component is mounted
         this.loadHeatData();
+        // Load the library attendance data when the component is mounted
+        this.loadLibraryAttendanceData();
     },
     methods: {
         loadHeatData() {
@@ -70,17 +84,126 @@ export default {
                 .then(module => {
                     // Once data is loaded, assign it to the heatData array
                     this.heatData = module.default.data;
-                    // console.log('成功加载热度数据:', this.heatData);
-                    // console.log('成功加载热度数据:', this.heatData[0][0], this.heatData[0][1]);
                 })
                 .catch(error => {
                     console.error('Error loading heat data:', error);
                 });
+        },
+        loadLibraryAttendanceData() {
+            // Load the JSON data asynchronously
+            let dateStr = `2023/${this.month}/${this.day}`;
+            import('../../../data/05-2023年全年实时在馆人数-2分钟一更新.json')
+                .then(module => {
+                    // Filter the data for January 1st
+                    this.libraryAttendanceData = module.RECORDS.filter(item => item.logTime.startsWith(dateStr));
+                    
+
+                    this.drawAttendanceChart();
+                })
+                .catch(error => {
+                    console.error('Error loading library attendance data:', error);
+                });
+        },
+        updateDataStr() {
+            this.dateStr = `2023/${this.month}/${this.day}`;
+            this.loadLibraryAttendanceData();
+        },
+        drawAttendanceChart() {
+            // 初始化 ECharts 实例
+            let attendanceChart = echarts.init(document.getElementById('attendanceChart'));
+            
+            // 准备数据
+            let timeData = this.libraryAttendanceData.map(item => {
+                // 找到空格，并取空格之后的部分
+                return item.logTime.split(' ')[1];
+            }); // 提取时间部分
+            let zgData = this.libraryAttendanceData.map(item => item.zg);
+            let lzdData = this.libraryAttendanceData.map(item => item.lzd);
+            let bygData = this.libraryAttendanceData.map(item => item.byg);
+            let xhskgData = this.libraryAttendanceData.map(item => item.xhskg);
+            
+            // 配置项
+            let option = {
+                title: {
+                    text: '1月1日实时在馆人数分布',
+                    left: 'center'
+                },
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                    data: ['主馆', '李政道图书馆', '包玉刚图书馆', '徐汇社科阅览室']
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: timeData,
+                    axisLabel: {
+                        formatter: function (value, index) {
+                            // 只显示部分时间刻度
+                            if (index % 6 === 0) {
+                                return value;
+                            } else {
+                                return '';
+                            }
+                        }
+                    }
+                }, 
+                yAxis: {
+                    type: 'value'
+                },
+                series: [
+                    {
+                        name: '主馆',
+                        type: 'line',
+                        data: zgData,
+                        smooth: true,
+                        areaStyle: {
+                            color: 'rgba(255, 0, 0, 0.5)' // 设置曲线下的面积颜色为半透明红色
+                        }
+                    },
+                    {
+                        name: '李政道图书馆',
+                        type: 'line',
+                        data: lzdData,
+                        smooth: true,
+                        areaStyle: {
+                            color: 'rgba(0, 0, 255, 0.5)' // 设置曲线下的面积颜色为半透明蓝色
+                        }
+                    },
+                    {
+                        name: '包玉刚图书馆',
+                        type: 'line',
+                        data: bygData,
+                        smooth: true,
+                        areaStyle: {
+                            color: 'rgba(0, 255, 0, 0.5)' // 设置曲线下的面积颜色为半透明绿色
+                        }
+                    },
+                    {
+                        name: '徐汇社科阅览室',
+                        type: 'line',
+                        data: xhskgData,
+                        smooth: true,
+                        areaStyle: {
+                            color: 'rgba(255, 165, 0, 0.5)' // 设置曲线下的面积颜色为半透明橙色
+                        }
+                    }
+                ]
+            };
+            
+            // 使用刚指定的配置项和数据显示图表
+            attendanceChart.setOption(option);
         }
     }
 }
 </script>
-
 
 <style>
 .bgpage{
